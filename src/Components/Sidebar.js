f4ae2cd9f3ed145b2089d5257a23848beb3c187b
Ben Tubby <b.tubby@fairfx.com>
2018-11-01 15:36:46 +0000

    stuff

--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -1,8 +1,59 @@
 import React from "react";
 
 import { push as M } from "react-burger-menu";
-import facebook_logo from "../Assets/facebook.png";
-import instagram_logo from "../Assets/instagram.png";
+// import facebook_logo from "../Assets/facebook.png";
+// import instagram_logo from "../Assets/instagram.png";
+import styled from "styled-components";
+
+const styles = {
+  bmBurgerButton: {
+    position: "fixed",
+    width: "36px",
+    height: "30px",
+    right: "36px",
+    top: "36px"
+  },
+  bmCrossButton: { 
+    height: "24px", 
+    width: "24px",
+    right: "35px",
+    top: "32px",
+  },
+  bmBurgerBars: { background: "#373a47" },
+  bmCross: { background: "#bdc3c7" },
+  bmMorphShape: { fill: "#fff63f" },
+  bmOverlay: { background: "rgba(0, 0, 0, 0.3)" }
+};
+
+const SidebarMenu = styled.div`
+  position: absolute;
+  top: 9px;
+  height: 30px;
+`;
+
+const MenuItem = styled.div`
+  font-family: 'Montserrat', sans-serif;
+  font-size: 1.3rem;
+  /* position: absolute; */
+  /* top: 27px; */
+  left: 20px;
+  z-index: 200;
+  font-weight: bold;
+  color: red;
+  @media (min-width: 768px) {
+    /* background: mediumseagreen; */
+    color: white;
+    font-size: 3rem;
+  }
+  a {
+    color: inherit;
+    text-decoration: none;
+
+    &:hover {
+      color: yellow;
+    }
+  }
+`;
 
 class Sidebar extends React.Component {
   showSettings(event) {
@@ -10,22 +61,7 @@ class Sidebar extends React.Component {
   }
 
   render() {
-    let styles = {
-      bmBurgerButton: {
-        position: "fixed",
-        width: "36px",
-        height: "30px",
-        right: "36px",
-        top: "36px"
-      },
-      bmBurgerBars: { background: "#373a47" },
-      bmCrossButton: { height: "24px", width: "24px" },
-      bmCross: { background: "#bdc3c7" },
-
-      bmMorphShape: { fill: "#fff63f" },
-
-      bmOverlay: { background: "rgba(0, 0, 0, 0.3)" }
-    };
+ 
     return (
       <M
         right
@@ -33,36 +69,42 @@ class Sidebar extends React.Component {
         pageWrapId={"page-wrap"}
         outerContainerId={"outer-container"}
       >
-        <div id="sidebar_bottom">
-          <a
+        <SidebarMenu>
+          {/* <a
             target="_blank"
             rel="noopener noreferrer"
             href="https://www.facebook.com/TenTonneSka?fref=ts"
-          >
+            >
             <img src={facebook_logo} alt="Logo" width="20%" />
-          </a>
-          <a target="_blank" 
+          </a> */}
+          {/* <a target="_blank" 
           rel="noopener noreferrer"
           href="#">
             <img src={instagram_logo} alt="Logo" width="20%" />
-          </a>
-        </div>
+          </a> */}
        
-        <a id="Live" className="menu-item" href="/">
-          LIVE
-        </a>
-        <a id="Media" className="menu-item" href="/media">
-          MEDIA
-        </a>
-        <a id="News" className="menu-item" href="/news">
-          CONTACT
-        </a>
-        <a href="http://tubbycreative.com/sounds/" target="_blank">
-          GEEZER
-        </a>
-        <a id="Test" className="menu-item" href="/test">
-          tmp
-        </a>
+        </SidebarMenu>
+        <MenuItem>
+          <a id="Live"  href="/">
+            LIVE
+          </a>
+        </MenuItem>
+        <MenuItem>
+          <a id="Media"  href="/media">
+            MEDIA
+          </a>
+        </MenuItem>
+        <MenuItem>
+          <a id="News"  href="/news">
+            CONTACT
+          </a>
+        </MenuItem>
+        <MenuItem>
+          <a id="News"  href="/geezer">
+            GEEZER
+          </a>
+        </MenuItem>
+
         {/* <a onClick={this.showSettings} className="menu-item--small" href="#">
           Settings
         </a> */}
